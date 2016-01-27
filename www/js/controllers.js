25db15cd5066a05e8824922486b797fd17f9755d
Carlos Amaral <carloslopesamaral@outlook.com>
2016-01-27 11:56:01 +0000

    ionic loading nas views a funcionar

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -3,81 +3,90 @@ angular.module('product.controllers', [])
 .controller('HomeCtrl', function($scope) {})
 
 .controller('PromocoesCtrl', function($scope, $http, $stateParams, $ionicLoading) {
-  var promocoes = [];
-  function Promocoes(){
-    $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/1.011/products?adapter=customer-adapter&from=60&size=10')
-      .then(function(response){
-        for (var i = 0; i < response.data.result.length; i++) {
-          promocoes.push({
-            name: response.data.result[i].name,
-            id: response.data.result[i].id,
-            brand: response.data.result[i].brand,
-            price: response.data.result[i].price,
-            currency: response.data.result[i].currency,
-            image: response.data.result[i].image,
-            hdpi:response.data.result[i].images.hdpi
-          });
-        }
-    $scope.promo = promocoes;
-      });
-  }
+  $ionicLoading.show({template: 'Carregando...'});
 
-  Promocoes();
+    function Promocoes(){
+      var promocoes = [];
+      $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/1.011/products?adapter=customer-adapter&from=60&size=10')
+        .then(function(response){
+          for (var i = 0; i < response.data.result.length; i++) {
+            promocoes.push({
+              name: response.data.result[i].name,
+              id: response.data.result[i].id,
+              brand: response.data.result[i].brand,
+              price: response.data.result[i].price,
+              currency: response.data.result[i].currency,
+              image: response.data.result[i].image,
+              hdpi:response.data.result[i].images.hdpi
+            });
+          }
+      $scope.promo = promocoes;
+      $ionicLoading.hide();
+        });
+
+    }
+    Promocoes();
 })
 
-.controller('CategoryCtrl', function($scope, $http) {
+.controller('CategoryCtrl', function($scope, $http, $ionicLoading) {
 
-  $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/1?adapter=customer-adapter').then(function (response) {
-
+  function viewCategories() {
+    $ionicLoading.show({template: 'Carregando...'});
     var categories = [];
-    for (var i = 0; i < response.data.result.length; i++) {
-      categories.push({
-        id: response.data.result[i].id,
-        name: response.data.result[i].name
-      });
-    }
-    $scope.cat = categories;
-  });
+    $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/1?adapter=customer-adapter').then(function (response) {
 
+      for (var i = 0; i < response.data.result.length; i++) {
+        categories.push({
+          id: response.data.result[i].id,
+          name: response.data.result[i].name
+        });
+      }
+      $scope.cat = categories;
+      $ionicLoading.hide();
+    });
+  }
+  viewCategories();
 })
 
 .controller('ProductCtrl', function ($scope, $http, $stateParams, $ionicLoading) {
 
-  var from = 0;
-  var products = [];
-  $scope.noMoreItemsAvailable = false;
-  $ionicLoading.show({template: 'Carregando...'});
+  function viewProduct() {
+    var from = 0;
+    var products = [];
+    $scope.noMoreItemsAvailable = false;
+    $ionicLoading.show({template: 'Carregando...'});
 
-  $scope.loadMore = function() {
+    $scope.loadMore = function () {
 
-    $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/' + $stateParams.id + '/products?adapter=customer-adapter&from=' + from + '&size=10')
-      .then(function (response) {
+      $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/hierarchicalstructure/' + $stateParams.id + '/products?adapter=customer-adapter&from=' + from + '&size=10')
+        .then(function (response) {
 
-      from += 10;
-      $scope.$broadcast('scroll.infiniteScrollComplete');
+          from += 10;
+          $scope.$broadcast('scroll.infiniteScrollComplete');
 
-      for (var i = 0; i < response.data.result.length; i++) {
-        products.push({
-          name: response.data.result[i].name,
-          id: response.data.result[i].id,
-          brand: response.data.result[i].brand,
-          price: response.data.result[i].price,
-          currency: response.data.result[i].currency,
-          image: response.data.result[i].image,
-          hdpi:response.data.result[i].images.hdpi,
-          xhdpi:response.data.result[i].images.xhdpi
+          for (var i = 0; i < response.data.result.length; i++) {
+            products.push({
+              name: response.data.result[i].name,
+              id: response.data.result[i].id,
+              brand: response.data.result[i].brand,
+              price: response.data.result[i].price,
+              currency: response.data.result[i].currency,
+              image: response.data.result[i].image,
+              hdpi: response.data.result[i].images.hdpi,
+              xhdpi: response.data.result[i].images.xhdpi
 
+            });
+          }
+          if (response.data.result.length == []) {
+            $scope.noMoreItemsAvailable = true;
+          }
+
+          $scope.pro = products;
+          $ionicLoading.hide();
         });
-      }
-      if (response.data.result.length == []) {
-        $scope.noMoreItemsAvailable = true;
-      }
-
-      $scope.pro = products;
-      $ionicLoading.hide();
-    });
-
-  };
+    };
+  }
+  viewProduct();
 
   $scope.$on('$stateChangeSuccess', function(){
     $scope.loadMore();
@@ -90,11 +99,12 @@ angular.module('product.controllers', [])
     $ionicLoading.show({template: 'Carregando...'});
     $http.get('http://52.17.194.187/orgs/monteserrat/apps/customer/products/' + $stateParams.id + '?adapter=customer-adapter')
       .then(function (response) {
-        $scope.prodetail = response.data.result;
+      $scope.prodetail = response.data.result;
+      $ionicLoading.hide();
       }, function (err) {
         console.log('err');
       });
-    $ionicLoading.hide();
+
 
 
     $scope.shareViaTwitter = function (prodetail) {
